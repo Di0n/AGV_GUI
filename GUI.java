94395d526ca8663758d7b6db5b43e5ca9e15f1de
Unknown <ralph@rouwen.com>
2017-12-22 11:22:45 +0100

    ook met versturen

--- GUI.java
+++ GUI.java
@@ -8,11 +8,15 @@ import javax.swing.JButton;
 
 public class GUI extends JFrame
 {
+    private ArrayList<Integer> data;
+
     private int width = 5;
     private int height = 5;
     private boolean firstClick = true;
     private int lastX;
     private int lastY;
+    private int secondLastX;
+    private int secondLastY;
 
     Cell field[][] = new Cell[25][25]; 
     ArrayList<ImageIcon> gridPhotos;
@@ -54,6 +58,7 @@ public class GUI extends JFrame
         buttons.add(route);
 
         makeGUI();
+        data = new ArrayList<Integer>();
 
         //Reset Grid
         resetButton.addActionListener(e-> 
@@ -62,18 +67,21 @@ public class GUI extends JFrame
                 gamePanel.revalidate();
                 makeGUI();
                 firstClick = true;
+                data.clear();
+
             });
 
         //Stuur Route
         route.addActionListener(e ->
             {
+                data.add(0x00);
+                System.out.println("einde");
+                // for(Integer command : data)
+                //     System.out.println(command);
 
             });
-
         setVisible(true);
-
     }
-
     private void makeGUI()
     {
         for(int y = 0; y < height; y++)
@@ -85,7 +93,8 @@ public class GUI extends JFrame
                 field[y][x] = new Cell(new JButton(""));
 
                 gamePanel.add(field[y][x].getButton());
-                field[y][x].getButton().addActionListener(e ->{ 
+                field[y][x].getButton().addActionListener(e ->
+                    { 
                         click(xx,yy);
                     });
             }
@@ -101,36 +110,133 @@ public class GUI extends JFrame
         if(firstClick == true)
         {
             field[y][x].getButton().setIcon(gridPhotos.get(1));
+            data.add(0x3e);
+            data.add(0x66);
+            System.out.println("begin");
+            System.out.println("F");
+
             lastX = x;
             lastY = y;
         }            
         else if(isNextToPrevious(x, y))
         {
             field[y][x].getButton().setIcon(gridPhotos.get(1));
+            makeRouteCommand(x, y);
+            secondLastX = lastX;
             lastX = x;
+            secondLastY = lastY;
             lastY = y;
         }
-        
 
         firstClick = false;
     }
-    
+
     private boolean isNextToPrevious(int x, int y)
     {
         boolean isNextTo = false;
-        if((x-1 == lastX) && (y == lastY) && x > 0 && field[y][x-1].getButton().getIcon().equals(gridPhotos.get(1)))
+        if((x-1 == lastX) && (y == lastY) && x > 0 )
+        {
             isNextTo = true;
-        else if((y-1 == lastY) && (x == lastX) && y > 0 && field[y-1][x].getButton().getIcon().equals(gridPhotos.get(1)))
+        }
+        else if((y-1 == lastY) && (x == lastX) && y > 0)
+        {    
             isNextTo = true;
-        else if((x+1 == lastX)&& (y == lastY) && x < 4 && field[y][x+1].getButton().getIcon().equals(gridPhotos.get(1)))
+        }
+        else if((x+1 == lastX)&& (y == lastY) && x < 4 )
+        {
             isNextTo = true;
-        else if((y+1 == lastY) && (x == lastX) && y < 4 && field[y+1][x].getButton().getIcon().equals(gridPhotos.get(1)))
+        }
+        else if((y+1 == lastY) && (x == lastX) && y < 4)
+        {
             isNextTo = true;
+        }
         else
             isNextTo = false;
-        
-        
+
         return isNextTo;
     }
 
+    private void makeRouteCommand(int x, int y)
+    {
+        Integer command;
+        if(isGoingStraight(x, y))
+        {
+            command = 0x66;
+            System.out.println("F");
+        }
+        else if(isGoingLeft(x, y))
+        {   
+            command = 0x72;
+            System.out.println("L");
+        }
+        else if(isGoingRight(x, y))
+        {
+            command = 0x6c;
+            System.out.println("R");
+        }
+        else
+        {
+            command = 0x66;
+            System.out.println("moet gefixt worden");
+            /* stap na de eerste kruising
+             * kan nog niet bepaald worden..
+             */ 
+        }    
+        data.add(command);
+        
+    }
+
+    private boolean isGoingStraight(int x, int y)
+    {
+        boolean straight = false;
+        if((x == lastX) && (x == secondLastX)) 
+            straight = true;
+        else if((y == lastY) && (y == secondLastY))
+            straight = true;
+        else
+            straight = false;
+        return straight;
+    }
+
+    
+    /*
+     * Volgende twee methodenamen zijn goed
+     * Variabelenamen in de methode zijn verkeerd
+     * Had ze verkeerdom geimplementeerd
+     * en heb geen zin om alles aan te passen..
+     */
+    
+    private boolean isGoingRight(int x, int y)
+    {
+        boolean left = false;
+        if(((x+1 == secondLastX) && (y+1 == secondLastY)) && (x == lastX))
+            left = true;
+        else if(((x-1 == secondLastX) && (y+1 == secondLastY)) && (y == lastY))
+            left = true;
+        else if(((x-1 == secondLastX) && (y-1 == secondLastY)) && (x == lastX))
+            left = true;
+        else if(((x+1 == secondLastX) && (y-1 == secondLastY)) && (y == lastY))
+            left = true;
+        else
+            left = false;
+
+        return left;
+    }
+
+    private boolean isGoingLeft(int x, int y)
+    {
+        boolean right = false;
+        if(((x+1 == secondLastX) && (y-1 == secondLastY)) && (x == lastX))
+            right = true;
+        else if(((x+1 == secondLastX) && (y+1 == secondLastY)) && (y == lastY))
+            right = true;
+        else if(((x-1 == secondLastX) && (y+1 == secondLastY)) && (x == lastX))
+            right = true;
+        else if(((x-1 == secondLastX) && (y-1 == secondLastY)) && (y == lastY))
+            right = true;
+        else
+            right = false;
+            
+        return right;
+    }
 }
